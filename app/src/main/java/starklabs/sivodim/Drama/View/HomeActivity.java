4b6c72e22a2512737aab2785c6c09156f2d1217c
Riccardo Rizzo <rizzoricky@gmail.com>
2016-05-30 01:49:34 +0200

    Aggiunge intent a FAB

--- app/src/main/java/starklabs/sivodim/Drama/View/HomeActivity.java
+++ app/src/main/java/starklabs/sivodim/Drama/View/HomeActivity.java
@@ -1,8 +1,8 @@
 package starklabs.sivodim.Drama.View;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import starklabs.sivodim.R;
 
@@ -29,8 +32,8 @@ public class HomeActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i=new Intent(HomeActivity.this,NewScreenplayActivity.class);
+                startActivity(i);
             }
         });
 
@@ -42,6 +45,12 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // content of the view
+        String[] citta=new String[]{"Star Wars","Lord of The Rings","Inside Out","Toy Story","District 9"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, R.layout.screenplay_item,citta);
+        ListView listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
     }
 
     @Override
@@ -81,13 +90,16 @@ public class HomeActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
+        Toast toast;
         if (id == R.id.nav_config) {
-            // Handle the camera action
+            toast = Toast.makeText(getApplicationContext(), "Apre l'applicazione di configurazione", Toast.LENGTH_SHORT);
+            toast.show();
         } else if (id == R.id.nav_guide) {
-
+            toast = Toast.makeText(getApplicationContext(), "Apre il manuale utente", Toast.LENGTH_SHORT);
+            toast.show();
         } else if (id == R.id.nav_info) {
-
+            toast = Toast.makeText(getApplicationContext(), "Apre info su app e autori", Toast.LENGTH_SHORT);
+            toast.show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
